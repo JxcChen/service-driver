855619a7aa95acf3063037b250ad86bc59f72f0a
陈金宣 <chenjinxuan1@gemdale.com>
2023-03-01 19:48:12 +0800

    新增har2case(未完成)

--- service_driver/har_parser.py
+++ service_driver/har_parser.py
@@ -7,18 +7,41 @@ import json
 import logging
 import os
 import sys
+from json import JSONDecodeError
+from urllib.parse import urlparse
 
 from service_driver.writer_content import write
 from service_driver.tenplate import Template
+from service_driver import utils
+
+# 忽略的请求头
+IGNORE_REQUEST_HEADERS = [
+    "host",
+    "accept",
+    "content-length",
+    "connection",
+    "accept-encoding",
+    "accept-language",
+    "origin",
+    "referer",
+    "cache-control",
+    "pragma",
+    "cookie",
+    "upgrade-insecure-requests",
+    ":authority",
+    ":method",
+    ":scheme",
+    ":path"
+]
 
 
 class HarParser:
 
     def __init__(self, har_file_path, exclude_url=None):
         self.har_file = har_file_path
-        self.exclude = exclude_url
+        self.exclude_url = exclude_url
 
-    def load_har_2_entry_json(self) -> str:
+    def load_har_2_entry_json(self) -> list[dict]:
         """
         load har data
         :return:
@@ -41,6 +64,176 @@ class HarParser:
             except (KeyError, TypeError):
                 sys.exit(1)
 
+    def __make_request_data(self, step_dict: dict, entry_json: dict):
+        """
+        将har中请求数据进行封装
+        :param entry_json:
+            entry_json (dict):
+                {
+                    "request": {
+                        "method": "POST",
+                        "postData": {
+                            "mimeType": "application/x-www-form-urlencoded; charset=utf-8",
+                            "params": [
+                                {"name": "a", "value": 1},
+                                {"name": "b", "value": "2"}
+                            }
+                        },
+                    },
+                    "response": {...}
+                }
+
+            Returns:
+                {
+                    "request": {
+                        "method": "POST",
+                        "data": {"v": "1", "w": "2"}
+                    }
+                }
+        """
+        method = entry_json['request'].get('method')
+        # 判断请求是否通过body进行传输
+        if method in ['POST','PUT','PATCH']:
+            post_data = entry_json['request'].get('postData',{})
+            mimetype:str = post_data.get('mimeType')
+
+            if 'text' in post_data:
+                data = post_data.get('text')
+            else:
+                params = post_data.get('params',[])
+                data = utils.covert_list_to_dict(params)
+            request_data_key = 'data'
+            # 判断是表单还是json
+            if not mimetype:
+                pass
+            elif mimetype.startswith("application/json"):
+                try:
+                    data = json.loads(data)
+                    request_data_key = 'json'
+                except JSONDecodeError:
+                    pass
+            elif mimetype.startswith("application/x-www-form-urlencoded"):
+                data = utils.convert_x_www_form_to_dict(data)
+
+            step_dict['request'][request_data_key] = data
+
+    def __make_request_method(self, step_dict, entry_json):
+        """
+        获取har中的请求方式
+        """
+        method = entry_json["request"].get("method")
+        if not method:
+            logging.exception("method missed in request.")
+            sys.exit(1)
+        step_dict['request']['method'] = method
+
+    def __make_request_headers(self, step_dict: dict, entry_json: dict):
+        """
+        获取har中对应header数据
+           :param entry_json:
+               entry_json (dict):
+                   {
+                       "request": {
+                           "headers": [
+                               {"name": "Host", "value": "httprunner.top"},
+                               {"name": "Content-Type", "value": "application/json"},
+                               {"name": "User-Agent", "value": "iOS/10.3"}
+                           ],
+                       },
+                       "response": {}
+                   }
+
+           Returns:
+               {
+                   "request": {
+                       headers: {"Content-Type": "application/json"}
+               }
+
+        """
+        step_headers = {}
+        for header in entry_json['request'].get('headers', []):
+            if header['name'].lower() in IGNORE_REQUEST_HEADERS:
+                # 直接忽略不进行处理
+                continue
+            step_headers[header['name']] = header['value']
+        if step_headers:
+            step_dict['request']['headers'] = step_headers
+
+    def __make_request_url(self, step_dict: dict, entry_json: dict):
+        """
+        将json数据中url的信息进行提取
+        :param entry_json:
+            entry_json (dict):
+                    {
+                        "request": {
+                            "url": "https://httprunner.top/home?v=1&w=2",
+                            "queryString": [
+                                {"name": "v", "value": "1"},
+                                {"name": "w", "value": "2"}
+                            ],
+                        },
+                        "response": {}
+                    }
+
+            Returns:
+                {
+                    "name: "/home",
+                    "request": {
+                        url: "https://httprunner.top/home",
+                        params: {"v": "1", "w": "2"}
+                    }
+                }
+        """
+        # 将url携带的参数进行分离
+        url_params = utils.covert_list_to_dict(entry_json["request"].get("queryString", []))
+        url = entry_json['request'].get('url')
+        if not url:
+            logging.exception('请求缺少url信息')
+            sys.exit(1)
+        # 将url进行拆解
+        parse_url = urlparse(url)
+        if url_params:
+            parse_url = parse_url._replace(query='')  # 清除url上的参数
+            step_dict["request"]["url"] = parse_url.geturl()
+            step_dict["request"]["params"] = url_params
+        else:
+            step_dict["request"]["url"] = parse_url.geturl()
+        step_dict['name'] = parse_url.path
+
+    def _gen_step(self, entry_json) -> dict:
+        """
+        将entry_json 转换成测试步骤需要用的的数据
+        :param entry_json:
+            entry_json (dict):
+                    {
+                        "request": {
+                            "method": "POST",
+                            "url": "https://httprunner.top/api/v1/Account/Login",
+                            "headers": [],
+                            "queryString": [],
+                            "postData": {},
+                        },
+                        "response": {
+                            "status": 200,
+                            "headers": [],
+                            "content": {}
+                        }
+                    }
+        :return:
+        """
+        step_dict = {
+            "name": "",
+            "request": {},
+            "validate": []
+        }
+        self.__make_request_url(step_dict, entry_json)
+        self.__make_request_headers(step_dict, entry_json)
+        self.__make_request_method(step_dict, entry_json)
+        self.__make_request_data(step_dict, entry_json)
+        self.__make_request_validate(step_dict, entry_json)
+
+        return step_dict
+
     def generate_testcase_steps(self, fmt_version) -> dict:
         """
         封装测试用例步骤
@@ -57,7 +250,13 @@ class HarParser:
             return False
 
         entry_json = self.load_har_2_entry_json()
-        print(entry_json)
+        test_steps = []
+        # 去除不需要的url
+        for entry in entry_json:
+            url = entry["request"].get("url")
+            if is_exclude_url(url, self.exclude_url):
+                continue
+            test_steps.append(self._gen_step(entry))
 
     def _make_testcase(self, fmt_version: str) -> str:
         """
---
+++ service_driver/utils.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+# @Time     :2023/3/1 11:08
+# @Author   :CHNJX
+# @File     :utils.py
+# @Desc     :public utils
+from urllib.parse import unquote
+
+
+def covert_list_to_dict(list_data):
+    """
+    将har list data 转换成dict
+    :param list_data
+        list_data (list)
+            [
+                {"name": "v", "value": "1"},
+                {"name": "w", "value": "2"}
+            ]
+
+        Returns:
+            dict:
+                {"v": "1", "w": "2"}
+    """
+    return {item['name']: item.get('value') for item in list_data}
+
+
+def convert_x_www_form_to_dict(form_data: str):
+    """
+    将form表单数据装换成dict字典数据
+    :param form_data:  (str): a=1&b=2
+    :return: (dict) {"a":1, "b":2}
+    """
+    if isinstance(form_data, str):
+        res_dict = {}
+        for kev_value in form_data.split('&'):
+            try:
+                key, value = kev_value.split('=')
+            except ValueError:
+                raise Exception(f"错误的 x_www_form_urlencoded 数据: {form_data}")
+            # unquote('abc%20def') -> 'abc def'.
+            res_dict[key] = unquote(value)
+        return res_dict
+    else:
+        return form_data
