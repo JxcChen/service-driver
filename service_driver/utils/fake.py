6fa8ded89561f3e91eda9ad22a88933a3304a083
陈金宣 <chenjinxuan1@gemdale.com>
2023-03-14 19:37:04 +0800

    优化代码

--- service_driver/utils/fake.py
+++ service_driver/utils/fake.py
@@ -41,72 +41,86 @@ class Fake:
     @classmethod
     def get_current_datetime(cls) -> str:
         return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    def get_random_name(self):
+    
+    @classmethod
+    def get_random_name(cls):
         """
         获取随机中文名
         :return:
         """
-        return self.fake.name()
+        return cls.fake.name()
 
-    def get_random_phone(self):
+    @classmethod
+    def get_random_phone(cls):
         """获取随机手机号"""
-        return self.fake.phone_number()
+        return cls.fake.phone_number()
 
-    def get_random_ssn(self):
+    @classmethod
+    def get_random_ssn(cls):
         """获取随机身份证号"""
-        return self.fake.ssn()
+        return cls.fake.ssn()
 
-    def get_random_credit_card_number(self):
+    @classmethod
+    def get_random_credit_card_number(cls):
         """获取随机银行卡号"""
-        return self.fake.credit_card_number()
+        return cls.fake.credit_card_number()
 
-    def get_random_address(self):
+    @classmethod
+    def get_random_address(cls):
         """获取随机地址"""
-        return self.fake.address().split(' ')[0]
+        return cls.fake.address().split(' ')[0]
 
-    def get_random_email(self):
+    @classmethod
+    def get_random_email(cls):
         """获取随机邮箱"""
-        return self.fake.email()
+        return cls.fake.email()
 
-    def get_random_date(self):
+    @classmethod
+    def get_random_date(cls):
         """获取随机日期"""
-        return self.fake.date()
+        return cls.fake.date()
 
-    def get_random_this_year_date(self):
+    @classmethod
+    def get_random_this_year_date(cls):
         """获取今年随机日期"""
-        return self.fake.date_this_year()
+        return cls.fake.date_this_year()
 
-    def get_random_date_time(self):
+    @classmethod
+    def get_random_date_time(cls):
         """获取随机时间"""
-        return self.fake.date_time()
+        return cls.fake.date_time()
 
-    def get_random_this_year_date_time(self):
+    @classmethod
+    def get_random_this_year_date_time(cls):
         """获取随机时间"""
-        return self.fake.date_time_this_year()
+        return cls.fake.date_time_this_year()
 
-    def get_random_future_date(self):
+    @classmethod
+    def get_random_future_date(cls):
         """获取未来的日期"""
-        return self.fake.future_date()
+        return cls.fake.future_date()
 
-    def get_random_future_date_time(self):
+    @classmethod
+    def get_random_future_date_time(cls):
         """获取未来的日期"""
-        return self.fake.future_datetime()
+        return cls.fake.future_datetime()
 
-    def get_customer_date(self, start_date: str, end_date: str):
+    @classmethod
+    def get_customer_date(cls, start_date: str, end_date: str):
         """
         获取自定义范围的日期
         :param start_date:  开始时间 1y为往后一年  -1y为往前一年
         :param end_date:  结束时间
         :return:
         """
-        return self.fake.date_between(start_date=start_date, end_date=end_date)
+        return cls.fake.date_between(start_date=start_date, end_date=end_date)
 
-    def get_customer_date_time(self, start_date: str, end_date: str):
+    @classmethod
+    def get_customer_date_time(cls, start_date: str, end_date: str):
         """
         获取自定义范围的时间
         :param start_date:  开始时间 +1y为往后一年  -1y为往前一年
         :param end_date:  结束时间
         :return:
         """
-        return self.fake.date_time_between(start_date=start_date, end_date=end_date)
+        return cls.fake.date_time_between(start_date=start_date, end_date=end_date)
